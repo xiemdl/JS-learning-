2d9f4289dacf8da36b389d39d61a7a2bf0767548
xiemdl <shielamadelanonuevodomingo@1804gmail.com>
2025-07-24 17:26:40 +0800

    main

---
+++ obj&ary(stdnt.rcd.sytm).js
@@ -0,0 +1,30 @@
+// //object
+// let person = {
+//   name: "Ana",
+//   age: 21,
+//   hobbies: ["reading", "eating"],
+//   isStudent: true
+
+// };
+
+// console.log(person.name);       // Dot notation
+// console.log(person["age"]);   
+// console.log(person["hobbies"]); // Bracket notation
+   
+// // Methods in Objects (Functions inside Objects)
+// let person1 = {
+//   name: "Ana",
+//   greet: function() {
+//     return "Hello, my name is " + this.name;
+//   }
+// };
+
+// console.log(person1.greet());
+
+let name = [Cris,Shiela,Nhalie];
+let student = {
+  name : [Cris,Shiela,Nhalie],
+  age : [24, 21, 2],
+  course : "BSIT"
+
+};
